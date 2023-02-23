def701fb1a14959dd67c9e6b71b9a39d7806e55b
markfialko <markfialkofialko@gmail.com>
2023-02-23 15:42:08 +0500

    added gitignore

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import authRouter from "./router/authRouter.js";
 import errorsMiddleware from "./middleware/errorsMiddleware.js";
 import ingredientRouter from "./router/ingredientRouter.js";
 
-const PORT = process.env.SERVER_PORT || config.get("SERVER_PORT")
+const PORT = process.env.PORT || config.get("SERVER_PORT")
 const DB_URL = process.env.DB_URL || config.get("DB_URL")
 const CLIENT_URL = process.env.CLIENT_URL ||  config.get("CLIENT_URL")
 
